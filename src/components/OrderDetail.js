5a458d13d026c4c00b90e4f69bb7e520c53d4f13
Nanouss <mrabet_nesrine@yahoo.fr>
2020-12-09 09:49:59 +0100

    OrderDetail component styled

--- src/components/OrderDetail.js
+++ src/components/OrderDetail.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import './OrderDetail.css';
 
 export default function OrderDetail() {
     return (
         <div className='orderDetail-component'>
-            <h2>Order Details</h2>
+            <h2 className='orderDetail-title'>Order Details</h2>
+
             <div className='detailed-order'>
                 <h3>Detailed order</h3>
                 <h4>Order No.</h4>
                 <h4>Status:</h4>
                 <h4>Placed on:</h4>
                 <button>Display items</button>
-            </div>
-            <div className='shipping-address'>
-                <h3>Shipping Address</h3>
+                <h3 className='shipping'>Shipping Address</h3>
                 <h4>Name:</h4>
                 <h4>Adress:</h4>
             </div>
+
             <div className='btn-order'>
-                <button>Reorder</button>
-                <button>Delete Order</button>
+                <button className='reorder-btn'>Reorder</button>
+                <button className='delete-btn'>Delete Order</button>
             </div>
         </div>
     )
