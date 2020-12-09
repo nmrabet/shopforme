6a9f19f77e394a7ff22a3805503fabd580fabd9e
Nanouss <mrabet_nesrine@yahoo.fr>
2020-12-09 10:09:34 +0100

    styled orderDetails component

--- src/components/OrderDetail.js
+++ src/components/OrderDetail.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import './OrderDetail.css';
+import ordersData from "../assets/ordersData.json";
 
 export default function OrderDetail() {
+    const ordersArray = ordersData.orders;
+    console.log(ordersArray);
     return (
         <div className='orderDetail-component'>
             <h2 className='orderDetail-title'>Order Details</h2>
 
             <div className='detailed-order'>
                 <h3>Detailed order</h3>
-                <h4>Order No.</h4>
-                <h4>Status:</h4>
-                <h4>Placed on:</h4>
+                <h5>Order No. {ordersArray.orderNo}</h5>
+                <h5>Status:</h5>
+                <h5>Placed on:{ordersArray.date}</h5>
                 <button>Display items</button>
                 <h3 className='shipping'>Shipping Address</h3>
-                <h4>Name:</h4>
-                <h4>Adress:</h4>
+                <h5>Name:{ordersArray.firstname} {ordersArray.lastname}</h5>
+                <h5>Adress: {ordersArray.address} </h5>
             </div>
 
             <div className='btn-order'>
